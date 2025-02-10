f382d2a93e2880639a3776770a8199f1195cae05
zadid15 <muhammadzadid061512@gmail.com>
2025-02-10 09:39:08 +0700

    fix bug on search input

--- src/pages/Browse.tsx
+++ src/pages/Browse.tsx
@@ -64,7 +64,7 @@ export default function Browse() {
 
         const delayDebounce = setTimeout(() => {
             fetchGames();
-        }, 0); // Dipercepat debounce-nya
+        }, 30); // Dipercepat debounce-nya
 
         return () => clearTimeout(delayDebounce);
     }, [searchQuery]);
